e432b271b1e4974d4fac8ab3cd9af66d27f43d8e
Hanieh <mohseni.hanieh@gmail.com>
2023-01-02 20:23:18 -0500

    Add Topbar, pages and components

---
+++ garderiefrontend/src/component/App.js
@@ -0,0 +1,35 @@
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+
+import Home from '../pages/Home';
+import Enrolment from '../pages/Enrolment';
+import Classes from '../pages/Classes';
+import Children from '../pages/Children';
+import Teachers from '../pages/Teachers';
+import Admin from '../pages/Admin';
+import Topbar from "./Topbar";
+
+
+
+
+
+
+
+function App() {
+  return (
+    <Router>
+        <Topbar/>
+       <Routes>
+       
+       <Route  path="/" element={<Home />}/>
+       <Route  path="/Enrolment" element={<Enrolment />}/>
+       <Route  path="/Classes" element={<Classes />}/>
+       <Route  path="/Childrens" element={<Children />}/>
+       <Route  path="/Teachers" element={<Teachers />}/>
+       <Route  path="/Admin" element={<Admin />}/>
+
+ </Routes>
+      </Router>
+  )
+}
+
+export default App
---
+++ garderiefrontend/src/pages/Enrolment.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Enrolment() {
+  return (
+    <div>Enrolment</div>
+  )
+}
+
+export default Enrolment
