b437e71d5f44c512c1beb6270394d6c7f4ad2eb6
Hanieh <mohseni.hanieh@gmail.com>
2023-01-06 23:48:03 -0500

    Add GetTeachers and ShowTeachers components for  the class page

---
+++ garderiefrontend/src/component/class/GetTeachers.js
@@ -0,0 +1,65 @@
+
+// Fetching teachers for class based on "ClassID" for search page
+//this component is one of the options of radio buttons
+// Tha other options will be activities and foods for each class
+
+import React from "react";
+import { useState } from "react";
+import styled, { StyledComponent } from "styled-components";
+import ShowFoods from "./ShowFoods";
+import ShowTeachers from "./ShowTeachers";
+
+// search = classId  we will get from user in main search page
+
+function GetTeachers() {
+  const [search, setSearch] = useState("");
+  const [MyTeacher, setTeacher] = useState([]);
+
+  const searchTeacher = (evt) => {
+    if (evt.key == "Enter") {
+      fetch(`http://localhost:8080//class/teachers/{search}`)
+        .then((res) => res.json())
+        .then((data) => {
+          //console.log(data.meals)
+          setTeacher(data.Food);
+        });
+    }
+  };
+
+  return (
+    <div className="main">
+      <div className="heading"></div>
+
+      <div className="searchBox">
+        <i class="fas fa-search"></i>
+        <input
+          type="search"
+          className="search-bar"
+          placeholder="Enter Your Class ID"
+          onChange={(e) => setSearch(e.target.value)}
+          value={search}
+          onKeyPress={searchTeacher}
+        ></input>
+      </div>
+
+      <div className="container">
+        {
+          //  MyTeacher is all the fetched Teachers from the api in BackEnd
+          // by map, I'm sending each of these in to ShowTeachers component that
+          //will show differnt feilds of Food like photo, Name,... in predefined structure
+
+          MyTeacher == null ? (
+            <p>Not Found</p>
+          ) : (
+            MyTeacher.map((res) => {
+              return <ShowTeachers data={res} />;
+            })
+          )
+        }
+      </div>
+    </div>
+  );
+}
+
+
+export default GetTeachers
--- garderiefrontend/src/component/class/ShowTeachers.js
+++ garderiefrontend/src/component/class/ShowTeachers.js
@@ -1,24 +1,25 @@
+
 //A template in which fields and fetched data from the api in backEnd are embedded in this structure
-// getActivity is coming from ShowActivities component and contains all the fields
+// getTeacher is coming from ShowTeacher component and contains all the fields
 
 import React from "react";
 import styled, { StyledComponent } from "styled-components";
 
-const ShowActivities = (getActivity) => {
+const ShowTeachers = (getTeacher) => {
   //console.log(getMeal.data)
   return (
     <div className="card">
-      <img src={getActivity.data.picPath} />
+      <img src={getTeacher.data.picPath} />
       <div className="info">
-        <h2>{getActivity.data.name}</h2>
-        <h2>{getActivity.data.activityDate}Activity Date</h2>
+        <h2>{getTeacher.data.name}</h2>
+        <h2>{getTeacher.data.offerDate}Offerd Date</h2>
       </div>
       <div className="recipe">
         <h2>Description</h2>
-        <p>{getActivity.data.description}</p>
+        <p>{getTeacher.data.description}</p>
       </div>
     </div>
   );
 };
 
-export default ShowActivities;
+export default ShowTeachers
