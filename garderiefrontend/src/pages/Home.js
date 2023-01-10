0ffb78c99c485701b4526883f08fd304a6fae634
Hanieh <mohseni.hanieh@gmail.com>
2023-01-10 17:52:28 -0500

    Add Home page changes in FrontEnd

--- garderiefrontend/src/pages/Home.js
+++ garderiefrontend/src/pages/Home.js
@@ -1,9 +1,27 @@
 import React from 'react'
+import HomeBackground from "../assets/HomeBackground.jpg"
+import styled from "styled-components"
+
 
 function Home() {
   return (
-    <div>Home</div>
+    <Homediv >
+    
+      Home
+
+    </Homediv>
   )
 }
+
+const Homediv = styled.div`
+height: calc(100vh - 50px);
+display:flex;
+flex-direction:column;
+align-items:center;
+justify-content:center;
+background-image:  url(${HomeBackground});
+
+
+`;
 
 export default Home
