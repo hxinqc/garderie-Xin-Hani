8c69526775171daa44d64daef05f26aa20088788
Hanieh <mohseni.hanieh@gmail.com>
2023-01-10 17:52:28 -0500

    Add Home page changes in FrontEnd

--- garderiefrontend/src/component/Topbar.js
+++ garderiefrontend/src/component/Topbar.js
@@ -1,96 +1,79 @@
-import React from 'react';
-import styled from 'styled-components';
-import { NavLink } from 'react-router-dom';
-import { Link } from 'react-router-dom';
-
+import React from "react";
+import styled from "styled-components";
+import { NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 function Topbar() {
   return (
     <Wrapper>
+      <TopCenter>
+        <NavigationList>
+          <LiDiv>
+            <NavigationLink to="/">HOME</NavigationLink>
+          </LiDiv>
 
-<TopCenter> 
+          <LiDiv>
+            <NavigationLink to="/Teachers">Teachers</NavigationLink>
+          </LiDiv>
 
-<NavigationList>
-   
-      <LiDiv>
-            <NavigationLink to="/" >HOME</NavigationLink>
-      </LiDiv>
+          <LiDiv>
+            <NavigationLink to="/Children">Children</NavigationLink>
+          </LiDiv>
 
-      <LiDiv>
-          <NavigationLink to="/Teachers">Teachers</NavigationLink>
-      </LiDiv>
+          <LiDiv>
+            <NavigationLink to="/Classes">Classes</NavigationLink>
+          </LiDiv>
 
-      <LiDiv>
-          <NavigationLink to="/Children">Children</NavigationLink>
-      </LiDiv>
+          <LiDiv>
+            <NavigationLink to="/Enrolment">Enrolment</NavigationLink>
+          </LiDiv>
 
-      <LiDiv>
-          <NavigationLink to="/Classes">Classes</NavigationLink>
-     </LiDiv>
+          <LiDiv>
+            <NavigationLink to="/Admin">Admin</NavigationLink>
+          </LiDiv>
 
-     <LiDiv>
-          <NavigationLink to="/Enrolment">Enrolment</NavigationLink>
-     </LiDiv>
-
-     <LiDiv>
-          <NavigationLink to="/Admin">Admin</NavigationLink>
-     </LiDiv>
-
-     <LiDiv>
-          <NavigationLink to="/Search">Search</NavigationLink>
-     </LiDiv>
-
- 
-
-    
-
-</NavigationList>
-
- </TopCenter>
-
+          <LiDiv>
+            <NavigationLink to="/Search">Search</NavigationLink>
+          </LiDiv>
+        </NavigationList>
+      </TopCenter>
     </Wrapper>
-    
-  )
+  );
 }
 
 const Wrapper = styled.div`
-width:100%;
-height: 50px;
-background-color: blue;
-color: white;
-position: sticky;
-top:0;
-display: flex;
-align-items: center;
-font-family: 'Josefin Sans', sans-serif;
-z-index: 999;
-
+  width: 100%;
+  height: 50px;
+  background-color: white;
+  color: white;
+  position: sticky;
+  top: 0;
+  display: flex;
+  align-items: center;
+  font-family: "Josefin Sans", sans-serif;
+  z-index: 999;
 `;
 
-const TopLeft= styled.div`
-flex: 3;
-display:flex;
-align-items:center;
-justify-content:center;
-
-
+const TopLeft = styled.div`
+  flex: 3;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
 
-const TopCenter= styled.div`
-flex:6;
-display: flex;
-margin:10;
-padding:0;
-list-style:none;
-
+const TopCenter = styled.div`
+  flex: 6;
+  display: flex;
+  margin: 10;
+  padding: 0;
+  list-style: none;
 `;
 
-const TopRight= styled.div`
-flex:3;
-display:flex;
-align-items:center;
-justify-content:center;
-
+const TopRight = styled.div`
+  flex: 3;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
 
 const NavigationList = styled.ul`
@@ -98,51 +81,47 @@ const NavigationList = styled.ul`
   display: flex;
 `;
 
-
 const LiDiv = styled.li`
   list-style-type: none;
-margin:20px;
-font-size:18px;
-font-weight: 300;
-cursor: pointer;
-
-`
+  margin: 20px;
+  font-size: 15px;
+  font-weight: 300;
+  cursor: pointer;
+  color: #57d7fc;
+`;
 const LeftLiDiv = styled.li`
   list-style-type: none;
-margin-right:1px;
-font-size:15px;
-font-weight: 300;
-cursor: pointer;
+  margin-right: 1px;
+  font-size: 10px;
+  font-weight: 300;
+  cursor: pointer;
+  color: #57d7fc;
+`;
 
-`
+const IconeDiv = styled.div`
+  font-size: 20px;
+  margin-right: 10px;
+  color: #444;
+  cursor: pointer;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
-
-const IconeDiv= styled.div`
-font-size: 20px;
-margin-right: 10px;
-color: #444;
-cursor:pointer;
-display: flex;
-justify-content:center;
-align-items:center;
-
-`
-
-const IDiv= styled.div`
-margin: 10px;
-
-`
-
+const IDiv = styled.div`
+  margin: 10px;
+`;
 
 const NavigationLink = styled(NavLink)`
   position: relative;
   text-decoration: none;
   padding: 0 16px;
+  color: #57d7fc;
   &.active {
-    color: black;
+    color: #fe4660;
   }
   &:after {
-    content: '';
+    content: "";
     position: absolute;
     background-color: currentColor;
     left: 0;
@@ -162,6 +141,4 @@ const NavigationLink = styled(NavLink)`
   }
 `;
 
-
-
-export default Topbar
+export default Topbar;
