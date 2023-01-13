e17dd9473ddaa953a165f02ed312845486a71458
Hanieh <mohseni.hanieh@gmail.com>
2023-01-13 02:18:55 -0500

    Add Teacher view and Edit component changes

--- garderiefrontend/src/component/teacher/ViewTeacher.js
+++ garderiefrontend/src/component/teacher/ViewTeacher.js
@@ -34,55 +34,134 @@ export default function ViewTeacher() {
   }, []);
 
   return (
+    <Wrapper>
+      <FormDiv>
     <Form>
-    <div style={{width:"600px", backgroundColor:"pink"}}>View Teacher:
-      <br />
+    <Mydiv >Selected Teacher Info </Mydiv>
+     
       <Label>
-        firstName: {firstName!=null?firstName:''} 
+        FirstName: {firstName!=null?firstName:''} 
         
       </Label>
       <br/>
       <Label>
-        lastName: {lastName!=null?lastName:''} 
+        LastName: {lastName!=null?lastName:''} 
       </Label>
       <br/>
       <Label>
-        isActive: {isActive?'true':'false'}
+        Active: {isActive?'Yes':'No'}
       </Label>
       <br/>
-      <Label>Message: {message} </Label>
+      <MessageLabel> {message} </MessageLabel>
 
-      <Link className="btn btn-primary mx-2" to={"/Teachers"} >Back to teachers</Link>
-    </div>
+
+     <Link to="/Teachers" style={{ textDecoration: 'none' }}>
+     <Button type="button"> Back </Button>
+ </Link>
+
+   
     </Form>
+    </FormDiv>
+
+    </Wrapper>
   )
 
 }
 
+const Mydiv=styled.div`
+color:white;
+display: flex;
+align-items: center;
+font-weight:900px;
+margin-bottom:30px;
+`;
+
+const Wrapper = styled.div`
+  height: calc(100vh - 60px);
+  z-index: -1;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  overflow-y: hidden;
+  overflow-x: hidden;
+  top: 50%;
+  left: 50%;
+`;
+
+const FormDiv = styled.div``;
+
 const Form = styled.form`
-    display: flex;
-    flex-direction: column;
-    padding: 25px;
-    margin: 15px;
-    border: none;
-    border-radius: 7px;
+  height: 380px;
+  width: 320px;
+  border-radius: 10px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background: rgba(0, 0, 22, 0.8);
+  padding: 60px;
+  margin: 15px;
+  border: none;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 1);
 `;
 const Label = styled.label`
+  align-items: center;
+  color:white;
+  display: block;
+`;
+
+const MessageLabel=styled.label`
+align-items: center;
+color:white;
+margin-left: 2px;
+display: block;
+font-weight: 400;
+color:white;
 `;
 
 const Button = styled.button`
-    width: 100px;
-    background-color: #fa9000;
-    color: #333;
-    border: none;
-    cursor: pointer;
-    padding: 10px;
-    font-size: 18px;
-    font-weight: 600;
-    margin-top: 10px;    
+  position: relative;
+  align-items: center;
+  margin-bottom: 20px;
+  display: block;
+  margin: 0 auto;
+  width: 80px;
+  background-color: #f9c000;
+  color: #333;
+  border: none;
+  cursor: pointer;
+  align-items: center;
+  padding: 3px;
+  font-weight: 300;
+  margin-top: 5px;
+  font-size: 15px;
+  border-radius: 30px;
+  box-shadow: 0 0 4px #f7dd00;
+  transition: box-shadow 0.5s ease;
 `;
 
 const Input = styled.input`
-    margin: 5px;
-    padding: 5px 15px;
+margin: 0 auto;
+  color: black;
+  padding: 5px 20px;
+  display: block;
+  width: 100%;
+  align-items: center;
+  margin-top: 5px;
+  background-color: white;
+  display: flex;
+  justify-content: right;
+  width: 230px;
+  margin-right: 12px;
+
+`;
+const Buttonsdiv = styled.div`
+display:flex;
+/* align-items:center; */
+margin-left: 10px;
+margin-right: 35px;
+margin-top: 10px;
 `;
