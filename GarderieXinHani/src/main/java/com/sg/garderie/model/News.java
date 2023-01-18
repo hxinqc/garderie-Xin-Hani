897e42ab7861d161bc4952092402277ee11d4aac
hxinqc <117105117+hxinqc@users.noreply.github.com>
2023-01-17 19:13:07 -0500

    Add News validation

--- GarderieXinHani/src/main/java/com/sg/garderie/model/News.java
+++ GarderieXinHani/src/main/java/com/sg/garderie/model/News.java
@@ -2,13 +2,18 @@ package com.sg.garderie.model;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 
 @Data
 public class News {
     private int id;
+    @NotNull(message = "Name is mandatory")
     private String name;
+    @NotNull(message = "IssueDate is mandatory")
     private LocalDate issueDate;
+    @NotBlank(message = "Picture is mandatory")
     private String picPath;
     private String content;
 
