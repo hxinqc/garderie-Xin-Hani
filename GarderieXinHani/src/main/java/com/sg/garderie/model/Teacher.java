eb9eb1a5299d027ece0502d58e5f78941f81f6ec
Hanieh <mohseni.hanieh@gmail.com>
2023-01-27 00:36:45 -0500

    Add validation for Teacher, Admin, Food model

--- GarderieXinHani/src/main/java/com/sg/garderie/model/Teacher.java
+++ GarderieXinHani/src/main/java/com/sg/garderie/model/Teacher.java
@@ -2,11 +2,16 @@ package com.sg.garderie.model;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotNull;
+
 @Data
 public class Teacher {
     private int ID;
+    @NotNull(message = "First Name is mandatory")
     private String firstName;
+    @NotNull(message = "Last Name is mandatory")
     private String lastName;
+    @NotNull(message = "Teacher status is mandatory")
     private boolean isActive;
 
     public boolean getIsActive() {
